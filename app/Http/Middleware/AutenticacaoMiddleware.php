4a9e702f1eeb33ccc588d943bf41d6f24b74dd86
Felipe <ficechin@hotmail.com>
2021-11-22 15:46:37 -0300

    Auth testing with global php session.

--- app/Http/Middleware/AutenticacaoMiddleware.php
+++ app/Http/Middleware/AutenticacaoMiddleware.php
@@ -16,26 +16,11 @@ class AutenticacaoMiddleware {
      */
     public function handle(Request $request, Closure $next, $metodo_autenticacao, $perfil) {
 
-        echo $metodo_autenticacao . ' - ' . $perfil;
-
-        if ($metodo_autenticacao == 'padrao') {
-            echo 'Verificar usuário e senha no banco de dados';
-        }
-
-        if ($metodo_autenticacao == 'ldap') {
-            echo 'Verificar o usuário e senha no AD';
-        }
-
-        if ($perfil == 'visitante') {
-            echo 'Exibir apenas alguns recursos';
-        } else {
-            echo 'Outros recursos';
-        }
-
-        if (false) {
+        session_start();
+        if (isset($_SESSION['email']) && $_SESSION['email'] != '') {
             return $next($request);
         } else {
-            return Response('Acesso negado! Rota exige autenticação');
+            return redirect()->route('site.login', ['erro' => 2]);
         }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\ClienteController;
 use App\Http\Controllers\ContatoController;
 use App\Http\Controllers\FornecedorController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\ProdutoController;
 use App\Http\Controllers\SobreNosController;
 use App\Http\Controllers\TesteController;
 use App\Http\Middleware\LogAcessoMiddleware;
@@ -31,21 +35,18 @@ Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos'])->name('site.so
 Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
 Route::post('/contato', [ContatoController::class, 'salvar'])->name('site.contato');
 
-Route::get('/login', function () {
-    return 'Login';
-})->name('site.login');
+Route::get('/login/{erro?}', [LoginController::class, 'index'])->name('site.login');
+Route::post('/login', [LoginController::class, 'autenticar'])->name('site.login');
 
 Route::middleware('autenticacao:padrao,visitante')->prefix('/app')->group(function () {
-    Route::get('/clientes', function () {
-        return 'Clientes';
-    })->name('app.clientes');
+    Route::get('/home', [HomeController::class, 'index'])->name('app.home');
+    Route::get('/sair', [LoginController::class, 'sair'])->name('app.sair');
+    Route::get('/cliente', [ClienteController::class, 'index'])->name('app.cliente');
 
-    Route::get('/fornecedores', [FornecedorController::class, 'index'])
-        ->name('app.fornecedores');
+    Route::get('/fornecedor', [FornecedorController::class, 'index'])
+        ->name('app.fornecedor');
 
-    Route::get('/produtos', function () {
-        return 'Produtos';
-    })->name('app.produtos');
+    Route::get('/produto', [ProdutoController::class, 'index'])->name('app.produto');
 });
 
 Route::get('/rota1', function () {
